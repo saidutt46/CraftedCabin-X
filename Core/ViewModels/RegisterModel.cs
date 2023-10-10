c0e5414e11591f45ddff14d509c4a669bc0efbc7
SAI DUTT <duttganduri46@gmail.com>
2023-10-09 20:55:22 -0700

    added basic auth func

---
+++ Core/ViewModels/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Special characters are not allowed in the User Name.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "User Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+    }
+}
---
+++ Core/ViewResponse/LoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core.DtoModels;
+
+namespace Core.ViewResponse
+{
+    public class LoginResponse
+    {
+        public string? Token { get; set; }
+        public DateTime Expiration { get; set; }
+        public UserProfileDto? UserProfile { get; set; }
+    }
+}
+
