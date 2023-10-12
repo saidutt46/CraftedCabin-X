4fa885549eae05f1b2c8e14600b7b776a6e97eaa
SAI DUTT <duttganduri46@gmail.com>
2023-10-11 20:40:50 -0700

    updated styling added color themes css to scss

--- WebAPI/ClientApp/src/app/app.component.ts
+++ WebAPI/ClientApp/src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ColorSchemeService } from '@services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private colorSchemeService: ColorSchemeService) {
+    // Load Color Scheme
+    this.colorSchemeService.load();
+  }
+
   title = 'app';
 }
 
--- WebAPI/ClientApp/src/app/components/navbar/navbar.component.ts
+++ WebAPI/ClientApp/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
+import { ColorSchemeService } from '@services';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  public themes = [
+    {
+        name: 'dark',
+        icon: 'brightness_3'
+    },
+    {
+        name: 'light',
+        icon: 'wb_sunny'
+    }
+  ];
+
+  constructor(private colorSchemeService: ColorSchemeService) { }
 
   userLogin() {
 
   }
 
   userRegister() {
+  }
+
+  updateTheme(theme: MatSelectChange) {
+    this.colorSchemeService.update(theme.value);
   }
 
 }
